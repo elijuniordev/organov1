e96e871211a324e3bf643b59dab7e0e638c5e0ef
Eli Junior <eli.junior3@aluno.unip.br>
2023-03-08 13:28:57 -0300

    Atualização para Typescript

---
+++ src/componentes/Rodape/index.tsx
@@ -0,0 +1,38 @@
+import './Rodape.css'
+
+const Rodape = () => {
+
+    return(
+        <footer className='footer'>
+            <section>
+                <ul>
+                    <li>
+                        <a href='facebook.com' target='_blank'>
+                            <img src='/imagens/fb.png' alt='Facebook'/>
+                        </a> 
+                    </li>
+                    <li>
+                        <a href='twitter.com' target='_blank'>
+                            <img src='/imagens/tw.png' alt='Twitter'/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='instagram.com' target='_blank'>
+                            <img src='/imagens/ig.png' alt='Instagram'/>
+                        </a>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <img src='/imagens/logo.png' alt='Logo da Organo'/>
+            </section>
+            <section>
+                <p>
+                    Desenvolvido por EliJunior.DEV
+                </p>
+            </section>
+        </footer>
+    )
+}
+
+export default Rodape
---
+++ src/componentes/Time/index.tsx
@@ -0,0 +1,43 @@
+import { IColaborador } from '../../compartilhado/interfaces/IColaborador';
+import Colaborador from '../Colaborador';
+import './Time.css'
+
+interface TimeProps {
+    corPrimaria: string;
+    nome: string;
+    colaboradores: IColaborador[]
+    corSecundaria: string;
+}
+
+const Time = (props: TimeProps) => {
+
+    const css = { backgroundColor: props.corSecundaria }
+
+    return(
+        (props.colaboradores.length > 0) ?
+        <section 
+            className='time' 
+            style={ css }
+        >
+            <h3
+                style={{ borderColor: props.corPrimaria }}
+            >
+                {props.nome}
+            </h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map(colaborador => 
+                    <Colaborador 
+                        key={colaborador.nome}
+                        corDeFundo={props.corPrimaria}
+                        nome={colaborador.nome}
+                        cargo={colaborador.cargo}
+                        imagem={colaborador.imagem}
+                    />
+                )}
+            </div>
+        </section>
+        : <></>
+    )
+}
+
+export default Time;
